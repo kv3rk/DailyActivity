f056fb46089e90809988686a9ff90008a12f4bb3
kv3rk <kverk07@gmail.com>
2026-05-17 04:31:58 +0300

    done accomplished goals and refreshing goals

---
+++ src/main/java/com/daily/plan/DailyPlan/GoalRefresh/RefreshingGoal.java
@@ -0,0 +1,12 @@
+package com.daily.plan.DailyPlan.GoalRefresh;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@EnableScheduling
+public class RefreshingRate {
+
+}
