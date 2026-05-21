a0459f2b052b4dcb1090b6dfacaf7eecfdb6f6ce
kv3rk <kverk07@gmail.com>
2026-05-21 08:05:19 +0300

    fix docker production setup 6.0

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE daily_goals (
-                             id UUID PRIMARY KEY,
-                             goal_date DATE,
-                             goal_text TEXT NOT NULL UNIQUE,
-                             done_flag BOOLEAN DEFAULT FALSE
-);
